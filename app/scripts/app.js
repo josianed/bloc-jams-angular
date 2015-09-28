061dad9890a915ea28b81e1f825a2a11ae69464a
jsudufour <jsudufour@gmail.com>
2015-09-28 08:37:00 -0400

    Added  and  configuration.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,16 @@
-var blocJams = angular.module('blocJams', []);
+var blocJams = angular.module('blocJams', ['ui.router']);
+
+var blocJams.config(function($stateProvider, $locationProvider) {
+    
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
+    
+    $stateProvider.state('album', {
+        url: '/album',
+        controller: 'Album.comtroller',
+        templateUrl: '/templates/album.html'
+    });
+    
+});
