4c13ddda06f08f1e7f00ec005d2edfd275d2a0e2
jsudufour <jsudufour@gmail.com>
2015-10-31 23:56:17 -0400

    First stab at assignment 4. Created controllers for Landing, Collection, and Album.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -27,3 +27,37 @@ blocJams.config(function($stateProvider, $locationProvider) {
     	});
     
 });
+
+
+//create controllers for landing, collection, and album
+blocJams.controller('LandingController', ['$scope', function($scope) {
+	$scope.title = "Turn the music up!";
+}]);
+
+
+blocJams.controller('CollectionController', ['$scope', function($scope) {
+	$scope.albumCollection = {
+		albums: [
+			{imgsrc: "assets/images/album_covers/01.png" , name: "The Colors", artist: "Pablo Picasso", songs: "X Songs"}
+		]
+	};
+
+}]);
+
+
+blocJams.controller('AlbumController', ['$scope', function($scope) {
+	$scope.albumPicasso = {
+	 name: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { name: 'Blue', length: 161.71, audioUrl: 'assets/music/blue' },
+         { name: 'Green', length: 103.96, audioUrl: 'assets/music/green' },
+         { name: 'Red', length: 268.45, audioUrl: 'assets/music/red' },
+         { name: 'Pink', length: 153.14, audioUrl: 'assets/music/pink' },
+         { name: 'Magenta', length: 374.22, audioUrl: 'assets/music/magenta' }
+     ]
+	};
+}]);
