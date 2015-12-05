b348bc5aa449335ad274dbe839cef66afb11d19e
jsudufour <jsudufour@gmail.com>
2015-12-05 18:48:07 -0500

    Refactored app.js with different syntax.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,82 +1,85 @@
-var blocJams = angular.module('blocJams', ['ui.router']);
+(function() {
+	function config($stateProvider, $locationProvider) {
+		$locationProvider
+			.html5Mode({
+				enabled: true,
+				requireBase: false
+		});
 
-blocJams.constant("CONFIG", {
-	ALBUMS: [{
-			name: 'The Colors 1',
-			artist: 'Pablo Picasso',
-			label: 'Cubism',
-			year: '1881',
-			albumArtUrl: 'assets/images/album_covers/01.png',
-			songs: [
-			{ name: 'Blue', length: 161.71, audioUrl: 'assets/music/blue' },
-			{ name: 'Green', length: 103.96, audioUrl: 'assets/music/green' },
-			{ name: 'Red', length: 268.45, audioUrl: 'assets/music/red' },
-			{ name: 'Pink', length: 153.14, audioUrl: 'assets/music/pink' },
-			{ name: 'Magenta', length: 374.22, audioUrl: 'assets/music/magenta' }
-			]
-		},
-		{
-			name: 'The Colors 2',
-			artist: 'Pablo Picasso',
-			label: 'Cubism',
-			year: '1881',
-			albumArtUrl: 'assets/images/album_covers/01.png',
-			songs: [
-			{ name: 'Blue', length: 161.71, audioUrl: 'assets/music/blue' },
-			{ name: 'Green', length: 103.96, audioUrl: 'assets/music/green' },
-			{ name: 'Red', length: 268.45, audioUrl: 'assets/music/red' },
-			{ name: 'Pink', length: 153.14, audioUrl: 'assets/music/pink' },
-			{ name: 'Magenta', length: 374.22, audioUrl: 'assets/music/magenta' }
-			]
-		},
-		{
-			name: 'The Colors 3',
-			artist: 'Pablo Picasso',
-			label: 'Cubism',
-			year: '1881',
-			albumArtUrl: 'assets/images/album_covers/01.png',
-			songs: [
-			{ name: 'Blue', length: 161.71, audioUrl: 'assets/music/blue' },
-			{ name: 'Green', length: 103.96, audioUrl: 'assets/music/green' },
-			{ name: 'Red', length: 268.45, audioUrl: 'assets/music/red' },
-			{ name: 'Pink', length: 153.14, audioUrl: 'assets/music/pink' },
-			{ name: 'Magenta', length: 374.22, audioUrl: 'assets/music/magenta' }
-			]
+		$stateProvider
+			.state('album', {
+				url: '/album/:id',
+				// params: {
+				// 	id: { value: '0' }
+				// },
+				templateUrl: '/templates/album.html',
+				controller: "AlbumController"
+			})
+
+			.state('collection', {
+				url: '/collection',
+				controller: 'CollectionController',
+				templateUrl: '/templates/collection.html'
+			})
+
+			.state('landing', {
+				url: '/landing',
+				controller: 'LandingController',
+				templateUrl: '/templates/landing.html'
+			});
 		}
-	]
-});
 
-blocJams.config(function($stateProvider, $locationProvider) {
+	angular
+		.module('blocJams', ['ui.router'])
+		.config(config);
 
-	$locationProvider.html5Mode({
-		enabled: true,
-		requireBase: false
-	});
+})();
 
-	$stateProvider
-	.state('album', {
-		url: '/album/:id',
-		// params: {
-		// 	id: { value: '0' }
-		// },
-		templateUrl: '/templates/album.html',
-		controller: "AlbumController",
-	
-	})
-
-	.state('collection', {
-		url: '/collection',
-		controller: 'CollectionController',
-		templateUrl: '/templates/collection.html'
-	})
-
-	.state('landing', {
-		url: '/landing',
-		controller: 'LandingController',
-		templateUrl: '/templates/landing.html'
-	});
-
-});
+// blocJams.constant("CONFIG", {
+// 	ALBUMS: [{
+// 			name: 'The Colors 1',
+// 			artist: 'Pablo Picasso',
+// 			label: 'Cubism',
+// 			year: '1881',
+// 			albumArtUrl: 'assets/images/album_covers/01.png',
+// 			songs: [
+// 			{ name: 'Blue', length: 161.71, audioUrl: 'assets/music/blue' },
+// 			{ name: 'Green', length: 103.96, audioUrl: 'assets/music/green' },
+// 			{ name: 'Red', length: 268.45, audioUrl: 'assets/music/red' },
+// 			{ name: 'Pink', length: 153.14, audioUrl: 'assets/music/pink' },
+// 			{ name: 'Magenta', length: 374.22, audioUrl: 'assets/music/magenta' }
+// 			]
+// 		},
+// 		{
+// 			name: 'The Colors 2',
+// 			artist: 'Pablo Picasso',
+// 			label: 'Cubism',
+// 			year: '1881',
+// 			albumArtUrl: 'assets/images/album_covers/01.png',
+// 			songs: [
+// 			{ name: 'Blue', length: 161.71, audioUrl: 'assets/music/blue' },
+// 			{ name: 'Green', length: 103.96, audioUrl: 'assets/music/green' },
+// 			{ name: 'Red', length: 268.45, audioUrl: 'assets/music/red' },
+// 			{ name: 'Pink', length: 153.14, audioUrl: 'assets/music/pink' },
+// 			{ name: 'Magenta', length: 374.22, audioUrl: 'assets/music/magenta' }
+// 			]
+// 		},
+// 		{
+// 			name: 'The Colors 3',
+// 			artist: 'Pablo Picasso',
+// 			label: 'Cubism',
+// 			year: '1881',
+// 			albumArtUrl: 'assets/images/album_covers/01.png',
+// 			songs: [
+// 			{ name: 'Blue', length: 161.71, audioUrl: 'assets/music/blue' },
+// 			{ name: 'Green', length: 103.96, audioUrl: 'assets/music/green' },
+// 			{ name: 'Red', length: 268.45, audioUrl: 'assets/music/red' },
+// 			{ name: 'Pink', length: 153.14, audioUrl: 'assets/music/pink' },
+// 			{ name: 'Magenta', length: 374.22, audioUrl: 'assets/music/magenta' }
+// 			]
+// 		}
+// 	]
+// });
 
 //example how to log messages to the console to debug
 // blocJams.controller("RyanController", [
@@ -90,178 +93,178 @@ blocJams.config(function($stateProvider, $locationProvider) {
 
 
 //create controllers for landing, collection, and album
-blocJams.controller('LandingController', [
-	'$scope',
-	'$log',
-	function($scope, $log) {
-		$log.debug("LandingController");
-		$scope.title = "Turn the music up!";
-	}
-]);
+// blocJams.controller('LandingController', [
+// 	'$scope',
+// 	'$log',
+// 	function($scope, $log) {
+// 		$log.debug("LandingController");
+// 		$scope.title = "Turn the music up!";
+// 	}
+// ]);
 
 
-blocJams.controller('CollectionController', ['$log', '$scope', 'CONFIG', function($log, $scope, CONFIG) {
-	$log.debug("CollectionController");
-	$scope.albumCollection = CONFIG.ALBUMS;
+// blocJams.controller('CollectionController', ['$log', '$scope', 'CONFIG', function($log, $scope, CONFIG) {
+// 	$log.debug("CollectionController");
+// 	$scope.albumCollection = CONFIG.ALBUMS;
 
-}]);
-
-
-blocJams.controller('AlbumController', [
-	'$scope',
-	'$log',
-	'$stateParams',
-	'SongPlayer',
-	'CONFIG',
-	function($scope, $log, $stateParams, SongPlayer, CONFIG) {
-		//alert("asdfadf");
-		$log.debug("AlbumController");
-		$log.debug("state params" + $stateParams);
-
-		$scope.album = CONFIG.ALBUMS[$stateParams.id];
-
-		$scope.playSong = function(songIndex) {
-			$log.debug("Song index: ", songIndex);
-			SongPlayer.play(songIndex);
-		};
-
-		// $scope.SongPlayer.play($scope.album.songs[0]);
-
-		$scope.pauseSong = function() {
-			SongPlayer.pause();
-		};
-
-		$scope.nextSong = function() {
-			SongPlayer.nextSong();
-		};
-
-		$scope.previousSong = function() {
-			SongPlayer.previousSong();
-		};
-
-		$scope.seek = function() {
-			SongPlayer.seek();
-		};
-	}
-]);
+// }]);
 
 
-blocJams.factory('SongPlayer', ['$stateParams', '$log', 'CONFIG', function($stateParams, $log, CONFIG) {
+// blocJams.controller('AlbumController', [
+// 	'$scope',
+// 	'$log',
+// 	'$stateParams',
+// 	'SongPlayer',
+// 	'CONFIG',
+// 	function($scope, $log, $stateParams, SongPlayer, CONFIG) {
+// 		//alert("asdfadf");
+// 		$log.debug("AlbumController");
+// 		$log.debug("state params" + $stateParams);
 
-	//Store the state of playing songs
-	var currentAlbum = CONFIG.ALBUMS[$stateParams.id];
-	var currentSongFromAlbum = {};
-	var currentSoundFile = null;
-	var currentVolume = 80;
+// 		$scope.album = CONFIG.ALBUMS[$stateParams.id];
 
-	var trackIndex = function(album, song) {
-			return album.songs.indexOf(song);
-	};
+// 		$scope.playSong = function(songIndex) {
+// 			$log.debug("Song index: ", songIndex);
+// 			SongPlayer.play(songIndex);
+// 		};
 
-	function setVolume(volume) {
-		if (currentSoundFile) {
-			currentSoundFile.setVolume(volume);
-		}
-	}
+// 		// $scope.SongPlayer.play($scope.album.songs[0]);
 
-	function setSong(songIndex) {
+// 		$scope.pauseSong = function() {
+// 			SongPlayer.pause();
+// 		};
 
-		currentSongFromAlbum = currentAlbum.songs[songIndex];
+// 		$scope.nextSong = function() {
+// 			SongPlayer.nextSong();
+// 		};
 
-		currentSoundFile = new buzz.sound(currentSongFromAlbum.audioUrl, {
+// 		$scope.previousSong = function() {
+// 			SongPlayer.previousSong();
+// 		};
 
-			formats: [ 'mp3' ],
-			preload: true
-		});
-
-		setVolume(currentVolume);
-
-	}
-
-
-
-	return {
-
-		// setSong: function(songIndex) {
-
-		// 	if (currentSongFromAlbum === null) {
-		// 		songIndex = 0;
-		// 	} 
-
-		// 	currentSongFromAlbum = currentAlbum.songs[songIndex];
-
-		// 	currentSoundFile = new buzz.sound(currentSongFromAlbum.audioUrl, {
-
-		// 		formats: [ 'mp3' ],
-		// 		preload: true
-		// 	});
-
-		// 	setVolume(currentVolume);
-
-		// },
-
-		play: function(songIndex) {
-			setSong(songIndex);
-			$log.debug(currentSoundFile);
-			currentSoundFile.play();
-		},
-
-		pause: function() {
-			currentSoundFile.pause();
-		},
-
-		nextSong: function() {
-
-			var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
-			currentSongIndex++;
-
-			if (currentSongIndex >= currentAlbum.songs.length) {
-				currentSongIndex = 0;
-			}
-
-			var getLastSongNumber = function(index) {
-				if (index === 0) {
-					return currentAlbum.songs.length;
-				} else {
-					return index;
-				}
-			};
-
-			setSong(currentSongIndex);
-			currentSoundFile.play();
-
-		},
+// 		$scope.seek = function() {
+// 			SongPlayer.seek();
+// 		};
+// 	}
+// ]);
 
 
-		previousSong: function() {
+// blocJams.factory('SongPlayer', ['$stateParams', '$log', 'CONFIG', function($stateParams, $log, CONFIG) {
 
-			var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
-			currentSongIndex--;
+// 	//Store the state of playing songs
+// 	var currentAlbum = CONFIG.ALBUMS[$stateParams.id];
+// 	var currentSongFromAlbum = {};
+// 	var currentSoundFile = null;
+// 	var currentVolume = 80;
 
-			if (currentSongIndex < 0) {
-				currentSongIndex = currentAlbum.songs.length - 1;
-			}
+// 	var trackIndex = function(album, song) {
+// 			return album.songs.indexOf(song);
+// 	};
 
-			var getLastSongNumber = function(index) {
-				if (index === currentAlbum.songs.length - 1) {
-					return 1;
-				} else {
-					return index + 2;
-				}
-			};
+// 	function setVolume(volume) {
+// 		if (currentSoundFile) {
+// 			currentSoundFile.setVolume(volume);
+// 		}
+// 	}
 
-			setSong(currentSongIndex);
-			currentSoundFile.play();
+// 	function setSong(songIndex) {
 
-		},
+// 		currentSongFromAlbum = currentAlbum.songs[songIndex];
+
+// 		currentSoundFile = new buzz.sound(currentSongFromAlbum.audioUrl, {
+
+// 			formats: [ 'mp3' ],
+// 			preload: true
+// 		});
+
+// 		setVolume(currentVolume);
+
+// 	}
 
 
-		seek: function(time) {
-			if (currentSoundFile) {
-				currentSoundFile.setTime(time);
-			}
-		}
 
-	}
+// 	return {
 
-}]);
+// 		// setSong: function(songIndex) {
+
+// 		// 	if (currentSongFromAlbum === null) {
+// 		// 		songIndex = 0;
+// 		// 	} 
+
+// 		// 	currentSongFromAlbum = currentAlbum.songs[songIndex];
+
+// 		// 	currentSoundFile = new buzz.sound(currentSongFromAlbum.audioUrl, {
+
+// 		// 		formats: [ 'mp3' ],
+// 		// 		preload: true
+// 		// 	});
+
+// 		// 	setVolume(currentVolume);
+
+// 		// },
+
+// 		play: function(songIndex) {
+// 			setSong(songIndex);
+// 			$log.debug(currentSoundFile);
+// 			currentSoundFile.play();
+// 		},
+
+// 		pause: function() {
+// 			currentSoundFile.pause();
+// 		},
+
+// 		nextSong: function() {
+
+// 			var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
+// 			currentSongIndex++;
+
+// 			if (currentSongIndex >= currentAlbum.songs.length) {
+// 				currentSongIndex = 0;
+// 			}
+
+// 			var getLastSongNumber = function(index) {
+// 				if (index === 0) {
+// 					return currentAlbum.songs.length;
+// 				} else {
+// 					return index;
+// 				}
+// 			};
+
+// 			setSong(currentSongIndex);
+// 			currentSoundFile.play();
+
+// 		},
+
+
+// 		previousSong: function() {
+
+// 			var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
+// 			currentSongIndex--;
+
+// 			if (currentSongIndex < 0) {
+// 				currentSongIndex = currentAlbum.songs.length - 1;
+// 			}
+
+// 			var getLastSongNumber = function(index) {
+// 				if (index === currentAlbum.songs.length - 1) {
+// 					return 1;
+// 				} else {
+// 					return index + 2;
+// 				}
+// 			};
+
+// 			setSong(currentSongIndex);
+// 			currentSoundFile.play();
+
+// 		},
+
+
+// 		seek: function(time) {
+// 			if (currentSoundFile) {
+// 				currentSoundFile.setTime(time);
+// 			}
+// 		}
+
+// 	}
+
+// }]);
