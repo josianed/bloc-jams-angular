ad3cc6075d6d81d734806f6e58517565975ed3d1
jsudufour <jsudufour@gmail.com>
2015-12-19 22:53:02 -0500

    WIP SongPlayer.pause function in SongPlayer.js

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -2,25 +2,41 @@
 	function SongPlayer() {
 		var SongPlayer = {};
 
+		var currentSong = null;
+		var currentBuzzObject = null;
+
 		SongPlayer.play = function(song) {
 			if (currentSong !== song) {
 				if (currentBuzzObject) {
 					currentBuzzObject.stop();
 					currentSong.playing = null;
+			} else if (currentSong === song) {
+				if (currentBuzzObject.isPaused()) {
+						currentBuzzObject.play();
 				}
-
-			var currentBuzzObject = new buzz.sound(song.audioUrl, {
-				formats: ['mp3'],
-				preload: true
-			});
-
-			currentSong = song;
-
-			currentBuzzObject.play();
-			song.playing = true;
-
 			}
 
+				var currentBuzzObject = new buzz.sound(song.audioUrl, {
+					formats: ['mp3'],
+					preload: true
+				});
+
+				currentSong = song;
+
+				currentBuzzObject.play();
+				console.log(currentBuzzObject);
+				song.playing = true;
+			}
+
+		};
+
+		console.log(currentBuzzObject);
+
+
+		SongPlayer.pause = function(song) {
+			currentBuzzObject.pause();
+			song.playing = false;
+			console.log(currentBuzzObject);
 		};
 
 		return SongPlayer;
