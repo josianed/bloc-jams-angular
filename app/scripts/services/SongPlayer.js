a8e16fda793f8d0b4d30eab46852c013bc054b4c
jsudufour <jsudufour@gmail.com>
2015-12-29 14:29:23 -0500

    Added documentation.

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -3,8 +3,18 @@
 		var SongPlayer = {};
 
 		var currentSong = null;
+
+		/**
+		* @desc Buzz object audio file
+		* @type {Object}
+		*/
 		var currentBuzzObject = null;
 
+		/**
+		* @function setSong
+		* @desc Stops currently playing song and loads new audio file as currentBuzzObject
+		* @param {Object} song
+		*/
 		var setSong = function(song) {
 			if (currentBuzzObject) {
 				currentBuzzObject.stop();
@@ -32,7 +42,7 @@
 				}
 			}
 
-		};
+		};S
 
 
 		SongPlayer.pause = function(song) {
