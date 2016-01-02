8073bbf4ec6b673fb8b6bd72b5026a826e2bdcaa
jsudufour <jsudufour@gmail.com>
2016-01-02 16:08:37 -0500

    Added logic to the seekBar directive, including updating the seek bar from a click event and a mousedown event.

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -1,5 +1,16 @@
 (function() {
-	function seekBar() {
+	function seekBar($document) {
+
+		var calculatePercent = function(seekBar, event) {
+		    var offsetX = event.pageX - seekBar.offset().left;
+		    var seekBarWidth = seekBar.width();
+		    var offsetXPercent = offsetX / seekBarWidth;
+		    offsetXPercent = Math.max(0, offsetXPercent);
+		    offsetXPercent = Math.min(1, offsetXPercent);
+		    return offsetXPercent;
+		};
+
+
 		return {
 			templateUrl: '/templates/directives/seek_bar.html',
 			replace: true,
@@ -9,6 +20,8 @@
 				scope.value = 0;
 				scope.max = 100;
 
+				var seekBar = $(element);
+
 				var percentString = function() {
 					var value = scope.value;
 					var max = scope.max;
@@ -17,13 +30,32 @@
 				};
 
 				scope.fillStyle = function() {
-					return: {width: percentString()};
+					return {width: percentString()};
 				};
+
+				scope.onClickSeekBar = function(event) {
+		            var percent = calculatePercent(seekBar, event);
+		            scope.value = percent * scope.max;
+		        };
+
+		        scope.trackThumb = function() {
+		        	$document.bind('mousemove.thumb', function(event) {
+		        		var percent = calculatePercent(seekBar, event);
+		        		scope.$apply(function() {
+		        			scope.value = percent * scope.max;
+		        		});
+		        	});
+
+			        $document.bind('mouseup.thumb', function() {
+			        	$document.unbind('mousemove.thumb');
+			        	$document.unbind('mouseup.thumb');
+			        });
+		        }
 			}
 		};
 	}
 
 	angular
 		.module('blocJams')
-		.directive('seekBar', seekBar);
+		.directive('seekBar', ['$document', seekBar]);
 })();
